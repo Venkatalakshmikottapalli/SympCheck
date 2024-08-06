9022ebc70e026da99a7b6c06e5d6c78b277988db
Venkatalakshmikottapalli <vkottapa@mail.yu.edu>
2024-08-06 18:02:43 -0400

    config change

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,3 +1,4 @@
 # app/utils/config.py
 
-API_URL = 'https://sympcheck-pred-3edef3c3f5c7.herokuapp.com'
+API_URL = 'https://sympcheck-pred-3edef3c3f5c7.herokuapp.com/predict'
+#API_URL = 'http://127.0.0.1:5000/predict'
--- app/utils/helper_functions.py
+++ app/utils/helper_functions.py
@@ -1,4 +1,6 @@
 # app/utils/helper_functions.py
+import json
+import requests
 
 import requests
 from .config import API_URL
@@ -8,11 +10,18 @@ def get_unique_symptoms(df):
 
 def predict_disease(symptoms):
     try:
+        # Manually create the JSON string
         symptoms_str = ', '.join(symptoms)
-        response = requests.post(API_URL, json={'symptoms': symptoms_str})
+        json_data = json.dumps({"symptoms": symptoms_str})  # Convert dictionary to JSON string
+        
+        # Send the request
+        response = requests.post(API_URL, data=json_data, headers={"Content-Type": "application/json", "Accept": "application/json"})
+        # Print the JSON string for debugging
+        print("request", json_data)  
         if response.status_code == 200:
             return response.json()['prediction']
         else:
+            print(f"Error: {response.status_code} - {response.text}")
             return "Prediction failed"
     except Exception as e:
         return str(e)
