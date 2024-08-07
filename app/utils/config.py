4645b91f11c205757622548a336868b5bf0b98b9
Venkatalakshmikottapalli <vkottapa@mail.yu.edu>
2024-08-06 22:34:22 -0400

    url update

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,4 +1,4 @@
 # app/utils/config.py
 
-API_URL = 'https://sympcheck-pred-3edef3c3f5c7.herokuapp.com/predict'
+API_URL = 'https://sympcheckbe-88b42835620c.herokuapp.com/predict'
 #API_URL = 'http://127.0.0.1:5000/predict'
