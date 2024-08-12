62c59193b76d594d2a4a6c264c7ff1d4f4af0c04
Venkatalakshmikottapalli <vkottapa@mail.yu.edu>
2024-08-11 20:57:29 -0400

    URL Update

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,6 +1,6 @@
 # app/utils/config.py
 
 # Production URL
-#API_URL = 'https://sympcheckbe-88b42835620c.herokuapp.com/predict' 
+API_URL = 'https://sympcheckbe-88b42835620c.herokuapp.com/predict' 
 # Testnet  URL
-API_URL = 'https://sympcheckbetestnet-fffc66f498ef.herokuapp.com/predict' 
+#API_URL = 'https://sympcheckbetestnet-fffc66f498ef.herokuapp.com/predict' 
